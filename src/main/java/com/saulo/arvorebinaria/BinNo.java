b0af7d64bd8139f3b70fa5fb6bdc5d978b13c997
sasasaulo <sasasaulo@gmail.com>
2022-02-18 15:09:53 -0300

    Árvore Binária

---
+++ src/main/java/com/saulo/arvorebinaria/BinNo.java
@@ -0,0 +1,50 @@
+package com.saulo.arvorebinaria;
+
+public class BinNo<T extends Comparable<T>> {
+
+    private T conteudo;
+    private BinNo<T> noEsq;
+    private BinNo<T> noDir;
+
+    public BinNo(T conteudo) {
+        this.conteudo = conteudo;
+        this.noEsq = this.noDir = null;
+    }
+
+    public BinNo () {
+
+    }
+
+    @Override
+    public String toString() {
+        return "BinNo{" +
+                "conteudo=" + conteudo +
+                ", noEsq=" + noEsq +
+                ", noDir=" + noDir +
+                '}';
+    }
+
+    public T getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(T conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public BinNo<T> getNoEsq() {
+        return noEsq;
+    }
+
+    public void setNoEsq(BinNo<T> noEsq) {
+        this.noEsq = noEsq;
+    }
+
+    public BinNo<T> getNoDir() {
+        return noDir;
+    }
+
+    public void setNoDir(BinNo<T> noDir) {
+        this.noDir = noDir;
+    }
+}
